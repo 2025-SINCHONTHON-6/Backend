2b86871d9ffbf267745c5e09aa64ed41a0be7c54
0euun <jeyejong70@naver.com>
2025-09-06 01:39:54 +0900

    TeaLogs 모델 생성

--- Dsinchonsix/challenges/models.py
+++ Dsinchonsix/challenges/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TeaLogs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tea_id = models.ForeignKey('teas.Tea', on_delete=models.CASCADE, null=False, verbose_name="차 아이디")
+    date = models.DateField(auto_now_add=True, verbose_name="차 마신 날짜")
+
+    def __str__(self):
+        return str(self.tea_id)
