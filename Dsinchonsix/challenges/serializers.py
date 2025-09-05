8843ac770ae56f179685a40cb21230a9af8d541d
0euun <jeyejong70@naver.com>
2025-09-06 02:09:28 +0900

    config url 설정

--- Dsinchonsix/challenges/serializers.py
+++ Dsinchonsix/challenges/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import TeaLog, Tea
+from .models import *
+from teas.models import Tea
 
 class TeaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,6 +12,6 @@ class TeaLogSerializer(serializers.ModelSerializer):
     tea_id = serializers.PrimaryKeyRelatedField(queryset=Tea.objects.all(), write_only=True, source='tea')
 
     class Meta:
-        model = TeaLog
+        model = TeaLogs
         fields = ('id', 'tea', 'tea_id', 'feeling', 'comment', 'created_at')
         read_only_fields = ('id', 'tea', 'created_at')
